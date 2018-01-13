403073ae3a6ccef4dfd422c55b24613d38b9ead3
adamsouthey <adamjsouthey@gmail.com>
2018-01-13 17:08:34 +0000

    flash messages displaying

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -17,7 +17,8 @@ function sessionsCreate(req, res, next) {
       req.user = user;
 
       req.flash('success', `Welcome back, ${user.username}!`);
-      res.redirect('/');
+      console.log('Hello!');
+      res.redirect('/dives');
     })
     .catch(next);
 }
